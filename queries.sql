50ad0174c30f04c5d25e0f2a7bfd86a4fcf6465b
Matvey Zhukov <158566674+matvey532@users.noreply.github.com>
2024-05-25 13:27:10 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,7 +8,7 @@ from customers;
 select
     concat(first_name, ' ', last_name) as seller,
     count(sales_id) as operations,
-    round(sum(price * quantity)) as income
+    floor(sum(price * quantity)) as income
 from employees as e
 inner join sales as s on e.employee_id = s.sales_person_id
 inner join products as p on s.product_id = p.product_id
@@ -27,7 +27,7 @@ with tab as (
 
 select
     concat(first_name, ' ', last_name) as seller,
-    round(avg(price * quantity)) as average_income
+    floor(avg(price * quantity)) as average_income
 from employees as e
 inner join sales as s on e.employee_id = s.sales_person_id
 inner join products as p on s.product_id = p.product_id
@@ -42,7 +42,7 @@ with tab as (
     select
         concat(first_name, ' ', last_name) as seller,
         to_char(sale_date, 'day') as day_of_week,
-        round(sum(price * quantity)) as income,
+        floor(sum(price * quantity)) as income,
         extract(isodow from sale_date) as day_number
     from employees as e
     inner join sales as s on e.employee_id = s.sales_person_id
@@ -87,7 +87,7 @@ order by age_category;
 select
     to_char(sale_date, 'YYYY-MM') as selling_month,
     count(distinct c.customer_id) as total_customers,
-    round(sum(quantity * price)) as income
+    floor(sum(quantity * price)) as income
 from sales as s
 inner join customers as c on s.customer_id = c.customer_id
 inner join products as p on s.product_id = p.product_id
